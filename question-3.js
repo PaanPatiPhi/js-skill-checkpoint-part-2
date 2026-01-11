b9cb5ab22ed2215b1a4029df91cbe4a4551280f7
phi <patiparn.phi@gmail.com>
2026-01-11 11:05:04 +0700

    fix: improve error handling and code structure in getUsers function with try and catch method

--- question-3.js
+++ question-3.js
@@ -1,11 +1,15 @@
 // เริ่มเขียนโค้ดตรงนี้
 async function getUsers() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await response.json();
-  let newUser = data.filter(user=> user.name.length > 17)
-  .map(user => user.name);
-  return console.log(newUser)
-  
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    let newUser = data
+      .filter((user) => user.name.length > 17)
+      .map((user) => user.name);
+    console.log(newUser);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getUsers();
